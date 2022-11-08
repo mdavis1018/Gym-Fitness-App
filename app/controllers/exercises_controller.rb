4aa4c53f68fbaee53b0e2c6fc40a07d401a6adb5
Marcus Davis <marcusdavis1018@gmail.com>
2022-11-08 14:38:24 -0500

    fixes exercises index action

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,6 +1,10 @@
 class ExercisesController < ApplicationController
   def index
   @exercises = Exercise.all
+  if params["category"]
+  category = Category.find_by(title: params["category"])
+  @exercises = category.exercises
+  end
   render json: @exercises
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
   resources :workout_details
   
   resources :categories
+  
 end
